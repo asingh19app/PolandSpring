563a22f0529c28f49d57b1f661fb130c1cd8f307
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2025-03-08 15:35:48 -0500

    Initial Commit

---
+++ PolandSpring/PolandSpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolandSpringApp.swift
+//  PolandSpring
+//
+//  Created by Adeshvir Dhanoa on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PolandSpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
